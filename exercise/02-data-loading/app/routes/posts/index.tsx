17488bbc822461ff591857fb44e2ea098f7a2228
Jeremy Marino <jermarino16@gmail.com>
2023-02-26 21:54:21 -0800

    Add render posts with fetch from model

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,27 @@
+import {json} from "@remix-run/node";
+import {Link, useLoaderData} from "@remix-run/react";
+import {getPosts} from "~/models/post.server";
+
+export const loader = async () => {
+    const posts = await getPosts();
+    return json(posts);
+};
+
 export default function Posts() {
-  return (
-    <main>
-      <h1>Posts</h1>
-    </main>
-  );
+    const posts = useLoaderData<typeof loader>();
+
+    return (
+        <main>
+            <h1>Posts</h1>
+            <ul>
+                {posts.map((post: any) => (
+                    <li key={post.slug}>
+                        <Link to={post.slug} className="text-blue-600 underline">
+                            {post.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
